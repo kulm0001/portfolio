f50d9403786ddee9d786b52b44211b133310fd99
kulm0001 <90398649+kulm0001@users.noreply.github.com>
2022-04-20 17:08:22 +0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+$('.nav-content .menu-toggle span.icon-menu').click(function() {
+    $('.nav-content .links').addClass('animate');
+})
+
+$('.nav-content .links span.icon-error').click(function() {
+    $('.nav-content .links').removeClass('animate');
+})
+
